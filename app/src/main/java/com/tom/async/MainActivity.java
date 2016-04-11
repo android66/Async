7e520c15f8b2cad9da8f3156c2c17c97ed4612f6
Hank Tom <hanktom223@gmail.com>
2016-04-11 10:58:29 +0800

    Job3Task練習

--- app/src/main/java/com/tom/async/MainActivity.java
+++ app/src/main/java/com/tom/async/MainActivity.java
@@ -58,6 +58,36 @@ public class MainActivity extends AppCompatActivity {
         }
     }
     public void go3(View v){
+        new Job3Task().execute(6);
+    }
+    class Job3Task extends AsyncTask<Integer, Integer, Void>{
+        private TextView info;
+        public Job3Task(){
+            info =  (TextView) findViewById(R.id.info);
+        }
+        @Override
+        protected Void doInBackground(Integer... params) {
+            for (int i=0; i<params[0]; i++){
+                publishProgress(i);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
 
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            info.setText(String.valueOf(values[0]));
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            info.setText("DONE");
+        }
     }
 }
