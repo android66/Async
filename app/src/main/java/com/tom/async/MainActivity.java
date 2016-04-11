90ddee264711594e3d76d1ba106de7e22aa396ae
Hank Tom <hanktom223@gmail.com>
2016-04-11 10:40:49 +0800

    Job1Task練習

--- app/src/main/java/com/tom/async/MainActivity.java
+++ app/src/main/java/com/tom/async/MainActivity.java
@@ -1,8 +1,10 @@
 package com.tom.async;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,7 +14,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
     public void go1(View v){
+        new Job1Task().execute();
+    }
+    class Job1Task extends AsyncTask<Void, Void, Void>{
 
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            TextView info = (TextView) findViewById(R.id.info);
+            info.setText("DONE");
+        }
     }
     public void go2(View v){
 
